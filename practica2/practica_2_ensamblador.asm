1d822da494c849d74f6037a292f87624998fc1e9
Edd-war <digiespirit.1807@gmail.com>
2021-08-12 09:21:01 -0500

    Comenzando practica 2

--- practica2/practica_2_ensamblador.asm
+++ practica2/practica_2_ensamblador.asm
@@ -1,21 +1,22 @@
-.stack 100h
+org 100h
 
 .data
 LF equ 10
 OD equ 13
 
  
-	opc1msj1 db 0ah, 0dh, 'Ingresa un numero para calcular su factorial: ', '$'
-	no_bal db 0ah, 0dh, 'Parentesis NO balanceados', '$'
-	bal db 0ah, 0dh, 'Parentesis SI balanceados', '$'
-	abiertos db 0
-	cerrados db 0
+	opc1msj1 db 13, 10, 'Ingresa un numero para calcular su factorial: ', '$'
+	;no_bal db 0ah, 0dh, 'Parentesis NO balanceados', '$'
+	;bal db 0ah, 0dh, 'Parentesis SI balanceados', '$'
+	;abiertos db 0
+	;cerrados db 0
     
-    mens db "Inserta cadena",LF,OD,"$"
-    salto db LF,OD,"$"
-    cadena label byte ;este programa acepta como máximo 50 caracteres introducidos
-    chare db 50
-    max db 50
+    numero dw ?
+    ;mens db "Inserta cadena",LF,OD,"$"
+    ;salto db LF,OD,"$"
+    ;cadena label byte ;este programa acepta como máximo 50 caracteres introducidos
+    ;chare db 50
+    ;max db 50
     entrada db ?
 
 .code
@@ -26,7 +27,7 @@ inicio:
     int 21h 
  
     mov bx, 0000h
-    lea SI, numero
+    lea si, numero
     mov cx, 2
   
     puntero_lector:
@@ -34,93 +35,46 @@ inicio:
     int 21h
     cmp al, 0dh
     jz convierte_numero_introducido
-    mov [si],al
-    inc si
+    mov ah, 0
+    push ax
+    ;inc si
     inc bx
-    loop puntero_lector          
-
-
+    cmp bx, 2
+    mov ah, 00
+    loop puntero_lector
+    pop ax          
+    sub al, 30h
+    mov numero, al
+    pop ax
+    ;mov al, [si]
+    ;xchg al, numero
+    sub al, 30h
+    mov bx, 10
+    mul bx
+    ;add al, numero
+    add numero, al
+    jmp continua
+    
     ;Funcion para convertir ASCII a hexadecimal y obtencion del cuadrado
     ;convierte_numero_introducido: macro
 	convierte_numero_introducido:
 	sub numero, 30h        
     mov al,numero
-    mul numero
-    mov cuadrado,al
+    
+    continua:
+    xor cx, cx
+    mov cl, numero
+    xor ax, ax
+    mov al, cl
+    factorial:
+    mul   
+    loop factorial
+         
+    mov ah,00h 
+    mov al,03h
+    int 10h
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    mov ax,@data
-    mov ds,ax
-
-    mov ah,09
-    lea dx,mens
-    int 21h
-
-    mov ah,0ah
-    lea dx, cadena
-    int 21h
-
-
-    mov ah,09
-    lea dx,salto
-    int 21h
-
-    lea si,entrada
-    mov cx,00
-             
-    pop bx
-    pop bx
-    mov bx, 0
-
-    analizar:
-    mov al,[si]
-    cmp al,0dh ;si detecta un enter
-    jz fin ;deja de contar los caracteres
-    inc cx
-    inc si
-    cmp al, 28h
-    jz acumula_abiertos
-    cmp al, 29h
-    jz extrae_abiertos
-    jmp analizar
-
-
-    acumula_abiertos:
-	push 28h
-	jmp analizar
-	
-
-	extrae_abiertos:
-	pop bx
-	cmp bl, 28h
-	jz analizar
-	
-	no_balanceados:
-	mov ah,09h
-    lea dx, no_bal
-    int 21h
-    
-    mov ax, 4C00h
-    int 21h
-
-	fin:
-	pop bx
-	cmp bl, 28h
-	jz no_balanceados
-	cmp bl, 29h
-    jz no_balanceados
-	mov ah,09h
-    lea dx, bal
-    int 21h
 
     mov ax, 4C00h
     int 21h
